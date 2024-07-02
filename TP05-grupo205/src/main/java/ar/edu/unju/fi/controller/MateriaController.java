a368c97e13152694a27ad03ba51b6b8e463df2e7
Esteban <Esteban@PC>
2024-07-02 19:04:48 -0300

    tp 05 Alan rodriguez

--- TP05-grupo205/src/main/java/ar/edu/unju/fi/controller/MateriaController.java
+++ TP05-grupo205/src/main/java/ar/edu/unju/fi/controller/MateriaController.java
@@ -1,6 +1,7 @@
 package ar.edu.unju.fi.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,67 +9,105 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.unju.fi.DTO.MateriaDTO;
+import ar.edu.unju.fi.service.CarreraService;
 import ar.edu.unju.fi.service.MateriaService;
+import jakarta.validation.Valid;
 
 @Controller
 public class MateriaController {
-	@Autowired
-	MateriaDTO nuevaMateria;
-	
-	@Autowired
-	MateriaService materiaService;
-	
-	@GetMapping("/formularioMateria")
-	public ModelAndView getFormMateria() {
-		ModelAndView modelView = new ModelAndView("materia/formMateria");
-		modelView.addObject("nuevaMateria",nuevaMateria);
-		
-		return modelView;
-	}
-	
-	@PostMapping("/guardarMateria")
-	public ModelAndView saveMateria(@ModelAttribute ("nuevaMateria") MateriaDTO Materia) {
-		
-		materiaService.guardarMateria(Materia);
-		ModelAndView modelView = new ModelAndView("materia/listadoDeMaterias");
-		modelView.addObject("listadoMaterias",materiaService.mostrarMaterias());
-		
-		return modelView;
-	}
-	
-	@GetMapping("/borrarMateria/{codigo}")
-	public ModelAndView borrarMateria(@PathVariable(name="codigo") String codigo){
-		materiaService.borrarMateria(codigo);
-		ModelAndView modelView = new ModelAndView("materia/listadoDeMaterias");
-		modelView.addObject("listadoMaterias",materiaService.mostrarMaterias());
-		
-		return modelView;
-	}
-	
-	@GetMapping("/darDeAlta/{codigo}")
-		public ModelAndView darDeAlta(@PathVariable(name="codigo") String codigo){
-		materiaService.darDeAlta(codigo);
-		ModelAndView modelView = new ModelAndView("materia/listadoDeMaterias");
-		modelView.addObject("listadoMaterias",materiaService.mostrarMaterias());
-		
-		return modelView;
-	}
-	
-	@GetMapping("/modificarMateria/{codigo}")
-	public ModelAndView mostrarFormularioModificarMateria(@PathVariable("codigo") String codigo){
-		ModelAndView modelView = new ModelAndView("materia/modificacionMateria");
-		modelView.addObject("Materia",materiaService.buscarMateria(codigo));
 
-		return modelView;
-	}
-	
-	@PostMapping("/guardarModificacionMateria")
-	public ModelAndView guardarModificacionMateria(@ModelAttribute ("Materia") MateriaDTO Materia) {
-		
-		materiaService.modificarMateria(Materia);
-		ModelAndView modelView = new ModelAndView("materia/listadoDeMaterias");
-		modelView.addObject("listadoMaterias",materiaService.mostrarMaterias());
-		
-		return modelView;
-	}
+    @Autowired
+    private MateriaDTO nuevaMateria;
+
+    @Autowired
+    private MateriaService materiaService;
+
+    @Autowired
+    private CarreraService cs;
+
+    @GetMapping("/formularioMateria")
+    public ModelAndView getFormMateria() {
+        ModelAndView modelView = new ModelAndView("materia/formMateria");
+        modelView.addObject("nuevaMateria", nuevaMateria);
+        modelView.addObject("listaCarreras", cs.MostrarCarrera());
+        modelView.addObject("band", false);
+        return modelView;
+    }
+
+    @GetMapping("/listadoMateria")
+    public ModelAndView getListadoMateria() {
+        ModelAndView modelView = new ModelAndView("materia/listadoDeMaterias");
+        modelView.addObject("listadoMaterias", materiaService.mostrarMaterias());
+        return modelView;
+    }
+
+    @GetMapping("/listadoMateriaInactiva")
+    public ModelAndView getListadoMateriaInactiva() {
+        ModelAndView modelView = new ModelAndView("materia/listaDeMateriasInactivos");
+        modelView.addObject("listadoMaterias", materiaService.mostrarMateriasInactivas());
+        return modelView;
+    }
+
+    @GetMapping("/listadoAlumnoMateria/{codigo}")
+    public ModelAndView getListadoAlumnoCarrera(@PathVariable(name = "codigo") String codigo) {
+        ModelAndView modelView = new ModelAndView("materia/listaDeAlumnosDeMateria");
+        modelView.addObject("materia", materiaService.buscarMateria(codigo));
+        return modelView;
+    }
+
+    @PostMapping("/guardarMateria")
+    public ModelAndView saveMateria(@Valid @ModelAttribute("nuevaMateria") MateriaDTO materia, BindingResult result) {
+        if (result.hasErrors()) {
+            ModelAndView modelView = new ModelAndView("materia/formMateria");
+            modelView.addObject("nuevaMateria", materia);
+            modelView.addObject("listaCarreras", cs.MostrarCarrera());
+            modelView.addObject("band", false);
+            return modelView;
+        }
+
+        materiaService.guardarMateria(materia);
+        return new ModelAndView("redirect:/listadoMateria");
+    }
+
+    @GetMapping("/darDeBajaAlumnoDeMateria/{codigoMateria}/{LU}")
+    public ModelAndView darDeBajaAlumnoDeMateria(@PathVariable(name = "codigoMateria") String codigoMateria, @PathVariable(name = "LU") String LU) {
+        materiaService.darDeBajaAlumno(codigoMateria, LU);
+        return new ModelAndView("redirect:/listadoAlumnoMateria/" + codigoMateria);
+    }
+
+    @GetMapping("/borrarMateria/{codigo}")
+    public ModelAndView borrarMateria(@PathVariable(name = "codigo") String codigo) {
+        materiaService.borrarMateria(codigo);
+        return new ModelAndView("redirect:/listadoMateria");
+    }
+
+    @GetMapping("/darDeAltaMateria/{codigo}")
+    public ModelAndView darDeAltaMateria(@PathVariable(name = "codigo") String codigo) {
+        materiaService.darDeAlta(codigo);
+        return new ModelAndView("redirect:/listadoMateriaInactiva");
+    }
+
+    @GetMapping("/modificarMateria/{codigo}")
+    public ModelAndView mostrarFormularioModificarMateria(@PathVariable("codigo") String codigo) {
+        ModelAndView modelView = new ModelAndView("materia/formMateria");
+        MateriaDTO materia = materiaService.buscarMateria(codigo);
+        modelView.addObject("nuevaMateria", materia);
+        modelView.addObject("listaCarreras", cs.MostrarCarrera());
+        modelView.addObject("band", true);
+        return modelView;
+    }
+
+    @PostMapping("/GuardarModMateria")
+    public ModelAndView guardarModificacionMateria(@Valid @ModelAttribute("nuevaMateria") MateriaDTO materia, BindingResult result) {
+        if (result.hasErrors()) {
+            ModelAndView modelView = new ModelAndView("materia/formMateria");
+            modelView.addObject("nuevaMateria", materia);
+            modelView.addObject("listaCarreras", cs.MostrarCarrera());
+            modelView.addObject("band", true);
+            return modelView;
+        }
+
+        materiaService.modificarMateria(materia);
+        return new ModelAndView("redirect:/listadoMateria");
+    }
 }
--- TP05-grupo205/src/main/java/ar/edu/unju/fi/model/Alumno.java
+++ TP05-grupo205/src/main/java/ar/edu/unju/fi/model/Alumno.java
@@ -1,38 +1,40 @@
 package ar.edu.unju.fi.model;
 
-
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import lombok.Data;
 
 @Data
-@Component
 @Entity
-public class Materia {
-	@Id
-    private String codigo;    
-    private String nombre;
-    private String curso;
-    private int cantidadHoras;
-    private String modalidad;
-    private Boolean estado;
+@Component
+public class Alumno {
 	
+	@Id
+	@Column(unique = true)
+    private String LU;
+	private String dni;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+    private Date fechaNacimiento;
+    private String domicilio;
+    private Boolean estado;
+    
     @ManyToOne
     @JoinColumn(name = "codigo_carrera")
     private Carrera carrera;
     
     @ManyToMany
-    private List<Alumno> alumnos = new ArrayList<Alumno>();
-    
-    @OneToOne
-    private Docente docente;
+    private List<Materia> Materias = new ArrayList<Materia>();
 }
